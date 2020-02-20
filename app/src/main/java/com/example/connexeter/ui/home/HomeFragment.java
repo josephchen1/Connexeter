204459a8e39f32e91bb2ffc877f0df8c4f3bad08
Joseph <jchen1@exeter.edu>
2020-02-20 12:50:07 -0500

    Modify ToDo

--- app/src/main/java/com/example/connexeter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/connexeter/ui/home/HomeFragment.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,11 +17,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.connexeter.MainActivity;
 import com.example.connexeter.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +39,18 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-
         return root;
+    }
+
+    public void onClick(View v){
+        switch(v.getId()) {
+            case R.id.add_btn:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+
+                Toast.makeText(getActivity(), "Item Added", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
