f9dab76ccee655ad7725bc9fc3cd8000f1d594f4
Joseph <jchen1@exeter.edu>
2020-02-21 12:09:36 -0500

    Modify todo

--- app/src/main/java/com/example/connexeter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/connexeter/ui/home/HomeFragment.java
@@ -13,14 +13,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.connexeter.MainActivity;
 import com.example.connexeter.R;
 
 import java.util.ArrayList;
@@ -30,10 +26,11 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private EditText itemET;
     private Button btn;
-    private ListView itemsList;
-
-    private ArrayList<String> items;
+    private ListView tasksList;
+    public TextView tasksNum;
+    private ArrayList<String> tasks;
     private ArrayAdapter<String> adapter;
+
 
 
     @Override
@@ -41,17 +38,20 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         btn = (Button) view.findViewById(R.id.add_btn);
-        itemET = view.findViewById(R.id.item_edit_text);
-        itemsList = view.findViewById(R.id.items_list);
+        itemET = view.findViewById(R.id.task_edit_text);
+        tasksList = view.findViewById(R.id.tasks_list);
 
-        items = FileHelper.readData(getActivity());
+        tasks = FileHelper.readData(getActivity());
 
-        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tasks);
 
-        itemsList.setAdapter(adapter);
+        tasksList.setAdapter(adapter);
 
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
+
+        tasksNum = (TextView) view.findViewById(R.id.tasks_num);
+        tasksNum.setText("Number of tasks: " + tasks.size());
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,19 +61,20 @@ public class HomeFragment extends Fragment {
                         String itemEntered = itemET.getText().toString();
                         adapter.add(itemEntered);
                         itemET.setText("");
-
-                        FileHelper.writeData(items, getActivity());
+                        tasksNum.setText("Number of tasks: " + tasks.size());
+                        FileHelper.writeData(tasks, getActivity());
 
                         Toast.makeText(getActivity(), "Item Added", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
         });
-        itemsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        tasksList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                items.remove(position);
+                tasks.remove(position);
                 adapter.notifyDataSetChanged();
+                tasksNum.setText("Number of tasks: " + tasks.size());
                 Toast.makeText(getActivity(), "Delete", Toast.LENGTH_SHORT).show();
             }
         });
