b6e14a0d074479061aa3993c8f44002ffe2bf69f
Dylan Shen <DylanShen@Dylans-MacBook-Pro-3.local>
2020-02-27 15:19:11 -0500

    Debugging DashboardFragment.java

--- app/src/main/java/com/example/connexeter/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/connexeter/ui/dashboard/DashboardFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -19,6 +20,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.connexeter.R;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -28,50 +30,54 @@ import java.util.Locale;
 public class DashboardFragment extends Fragment {
 
     DashboardViewModel dashboardViewModel;
-    ArrayList<addOLS> formatList;
-    private RecyclerView className;
-    private RecyclerView classLevel;
-    private RecyclerView roomNumber;
-    private RecyclerView teacherName;
-    private RecyclerView format;
+
+    RecyclerView recyclerView;
+    addOLSAdapter adapter;
+
+    List<addOLS> addOLSList;
+
+
+//    private RecyclerView className;
+//    private RecyclerView classLevel;
+//    private RecyclerView roomNumber;
+//    private RecyclerView teacherName;
+//    private RecyclerView format;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        RecyclerView allFormats = root.findViewById(R.id.allFormats);
-        allFormats.setLayoutManager(new LinearLayoutManager(getActivity()));
+//ols thing
+        addOLSList = new ArrayList<>();
 
-        OlsAdapter adapter = new OlsAdapter(OlsAdapter.allFormats);
-        allFormats.setAdapter(adapter);
+        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        addOLSList.add(
+                new addOLS(1, "A", "Math", "MAT-420",
+                        "ACB-220", "Mr.Chen")
+        );
+        addOLSList.add(
+                new addOLS(1,"B","Biology","BIO-320",
+                        "PHP-302", "Mr. Matlack")
+        );
+        adapter = new addOLSAdapter(getActivity(),addOLSList);
+        recyclerView.setAdapter(adapter);
 
+        //date and time
         final String currentDate = new SimpleDateFormat("MMM, dd, yyyy", Locale.getDefault()).format(new Date());
         final TextView date = root.findViewById(R.id.date);
         date.setText(currentDate);
 
+        //dropdown menu
         Spinner setWeekNumber = (Spinner) root.findViewById(R.id.weekNumber);
-
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.reply_entries));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         setWeekNumber.setAdapter(myAdapter);
 
-//        setWeekNumber.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()) {
-//            @Override
-//                    public void onItemSelectedListener(AdapterView<?> adapterView, View view, int i, long l);
-//        }
+
         return root;
     }
-
-    private ArrayList<addOLS> allClasses() {
-        ArrayList<addOLS> list = new ArrayList<>();
-        list.add(new addOLS(1, "A", "Math", "MAT-202",
-                "ACA323", "Dylan"));
-        list.add(new addOLS(1, "B", "English", "ENG-520",
-                "ACB-204", "Shen"));
-
-        return list;
-    }
-
 
 }
